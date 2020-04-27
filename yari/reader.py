2344ee0a4bc781becebf4324a17846645777c1e2
mtaylor33 <taylormjm3121@gmail.com>
2020-04-27 00:22:48 -0400

    Updated exception class name and added the NodeNotFound exception.

--- yari/reader.py
+++ yari/reader.py
@@ -2,11 +2,17 @@ import os
 import yaml
 
 
+class NodeNotFound(Exception):
+    """Raised when YAML node block "table" cannot be found."""
+
+
 class QueryNotFound(Exception):
-    """Raised when an unsuccessful query is made."""
+    """Raised when a query cannot be found."""
 
 
 class ReaderQueryResult:
+    """Handles successful query result."""
+
     def __init__(self, data_source: dict, data_table: str) -> None:
         self.data_source = data_source.get(data_table)
 
@@ -27,28 +33,40 @@ class ReaderQueryResult:
             if query in self.data_source:
                 self.data_source = self.data_source[query]
             else:
-                raise QueryNotFound(f"error: specified query unsuccessful '{query}'")
+                raise QueryNotFound(f"specified query unsuccessful '{query}'")
         return self.data_source
 
 
 def _load(data_file: str) -> ReaderQueryResult:
-    """Returns ReaderQueryResult on success or raises exception on error."""
+    """Loads ReaderQueryResult object on success or raises exception on error.
+
+    Args:
+        data_file (str): YAML file to read from (file extension is not needed).
+
+    """
     data = os.path.join(os.path.dirname(__file__), f"sources/{data_file}.yml")
     if not os.path.exists(data):
-        raise FileNotFoundError(f"error: cannot load YAML file from '{data}'")
+        raise FileNotFoundError(f"cannot load YAML file from '{data}'")
 
     data_source = yaml.load(open(data), Loader=yaml.FullLoader)
     if data_file not in data_source:
-        raise KeyError(f"error: invalid table specified '{data_file}'")
+        raise NodeNotFound(f"invalid table specified '{data_file}'")
     else:
         return ReaderQueryResult(data_source, data_file)
 
 
 def reader(data_file: str, query_values=None) -> (dict, list):
+    """Loads and reads from the requested data_file using query_values.
+
+    Args:
+        data_file (str): YAML file to read from (file extension is not needed).
+        query_values (None, tuple): Chain of values used to query YAML elements.
+
+    """
     try:
         rd = _load(data_file)
         return rd.fetch(query_values)
-    except (FileNotFoundError, KeyError, TypeError,) as error:
+    except (FileNotFoundError, NodeNotFound, TypeError,) as error:
         exit(error)
     except QueryNotFound:
         pass
