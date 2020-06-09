726998c78cb191501c365c17a51f7063f0e5cdc8
mtaylor33 <taylormjm3121@gmail.com>
2020-06-09 14:14:44 -0400

    Added ProficiencyTypeValueError exception.

--- yari/proficiency.py
+++ yari/proficiency.py
@@ -1,6 +1,10 @@
 from yari.collect import fuse
 
 
+class ProficiencyTypeValueError(ValueError):
+    """Raised if invalid proficiency type specified."""
+
+
 class ProficiencyGenerator:
     """Merges class with racial proficiencies (if applicable)."""
 
@@ -12,7 +16,7 @@ class ProficiencyGenerator:
             traits (dict): Racial proficiency by prof_type (if applicable).
         """
         if prof_type not in ("armors", "tools", "weapons"):
-            raise ValueError(f"invalid 'prof_type' argument '{prof_type}'")
+            raise ProficiencyTypeValueError(f"invalid 'prof_type' argument '{prof_type}'")
         else:
             class_proficiency = features.get("proficiency").get(prof_type)
             if "proficiency" in traits:
