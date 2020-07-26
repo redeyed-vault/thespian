8ffebf4afaddb27026884f1c827dea67acfc9429
mtaylor33 <taylormjm3121@gmail.com>
2020-07-26 04:21:42 -0400

    Removed SkillGenerator class since functionality has been transferred elsewhere.

--- yari/skills.py
+++ yari/skills.py
@@ -1,45 +1,4 @@
-import random
-
 from yari.loader import _read
-
-
-class SkillGenerator:
-    """Generates a random skill set by background and klass."""
-
-    def __init__(self, background: str, klass: str, racial_skills: list) -> None:
-        """
-        Args:
-            background (str): Character background.
-            klass (str): Character's class.
-            racial_skills (list): Character's skills.
-        """
-        background_skills = get_background_skills(background)
-        class_skills = [
-            skill
-            for skill in self.get_skills_by_class(klass)
-            if skill not in racial_skills and skill not in background_skills
-        ]
-        generated_skills = racial_skills + background_skills
-
-        if klass in ("Rogue",):
-            skill_allotment = 4
-        elif klass in ("Bard", "Ranger"):
-            skill_allotment = 3
-        else:
-            skill_allotment = 2
-        generated_skills = generated_skills + random.sample(class_skills, skill_allotment)
-        self.skills = generated_skills
-
-    @staticmethod
-    def get_skills_by_class(klass: str):
-        """Returns a list of skills by klass.
-
-        Args:
-            klass (str): Class to get skill list for.
-        """
-        for skill in _read(file="skills"):
-            if klass in _read(skill, "classes", file="skills"):
-                yield skill
 
 
 def get_all_skills() -> list:
