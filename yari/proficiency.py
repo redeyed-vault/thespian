fada90cfe4be982bb4ea8476b4bf0e2f5cadf125
mtaylor33 <taylormjm3121@gmail.com>
2020-08-11 04:30:19 -0400

    Added get_tool_chest and get_weapon_chest from improvement.py as standalone functions. Added get_armor_chest function.

--- yari/proficiency.py
+++ yari/proficiency.py
@@ -1,3 +1,6 @@
+from yari.loader import _read
+
+
 class ProficiencyTypeValueError(ValueError):
     """Raised if invalid proficiency type specified."""
 
@@ -19,8 +22,31 @@ class ProficiencyGenerator:
                 f"Invalid 'prof_type' argument '{prof_type}' specified."
             )
 
-        race_proficiency = [x for x in race_proficiency if x not in class_proficiency]
+        race_proficiency = [p for p in race_proficiency if p not in class_proficiency]
         if len(race_proficiency) is not 0:
             self.proficiency = class_proficiency + race_proficiency
         else:
             self.proficiency = class_proficiency
+
+
+def get_armor_chest():
+    """Returns a full collection of armors."""
+    pass
+
+
+def get_tool_chest():
+    """Returns a full collection of tools."""
+    for main_tool in _read(file="tools"):
+        if main_tool in ("Artisan's tools", "Gaming set", "Musical instrument"):
+            for sub_tool in _read(main_tool, file="tools"):
+                yield f"{main_tool} - {sub_tool}"
+        else:
+            yield main_tool
+
+
+def get_weapon_chest():
+    """Returns a full collection of weapons."""
+    weapon_chest = dict()
+    for weapon_category in ("Simple", "Martial"):
+        weapon_chest[weapon_category] = _read(weapon_category, file="weapons")
+    yield weapon_chest
