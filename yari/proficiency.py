2d63fb473c4832feb6c9d4755ee0a26d0ce929c3
mtaylor33 <taylormjm3121@gmail.com>
2020-08-27 00:30:50 -0400

    Implementing different implementation of loader.

--- yari/proficiency.py
+++ yari/proficiency.py
@@ -30,7 +30,7 @@ def get_armor_chest():
     """Returns a full collection of armors."""
     armor_chest = dict()
     for armor_category in ("Heavy", "Light", "Medium"):
-        armor_chest[armor_category] = _read(armor_category, file="armors")
+        armor_chest[armor_category] = [p for p in _read(armor_category, file="armors")][0]
     yield armor_chest
 
 
@@ -38,7 +38,7 @@ def get_tool_chest():
     """Returns a full collection of tools."""
     for main_tool in _read(file="tools"):
         if main_tool in ("Artisan's tools", "Gaming set", "Musical instrument"):
-            for sub_tool in _read(main_tool, file="tools"):
+            for sub_tool in [t for t in _read(main_tool, file="tools")][0]:
                 yield f"{main_tool} - {sub_tool}"
         else:
             yield main_tool
@@ -48,5 +48,5 @@ def get_weapon_chest():
     """Returns a full collection of weapons."""
     weapon_chest = dict()
     for weapon_category in ("Simple", "Martial"):
-        weapon_chest[weapon_category] = _read(weapon_category, file="weapons")
+        weapon_chest[weapon_category] = [w for w in _read(weapon_category, file="weapons")][0]
     yield weapon_chest
