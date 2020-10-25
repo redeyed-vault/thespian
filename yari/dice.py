2ac0fa6feefda7534fe28ff96ebb7b9b8c7d4a4d
mtaylor33 <taylormjm3121@gmail.com>
2020-10-25 03:51:38 -0400

    Fixed exception error messages.

--- yari/dice.py
+++ yari/dice.py
@@ -13,17 +13,17 @@ def roll(string: str):
         raise TypeError("Argument 'string' must be of type 'str'.")
     else:
         if not re.search("[0-9]d[0-9]", string):
-            raise ValueError("Argument 'string' has an invalid format.")
+            raise ValueError("Argument 'string' has an invalid format (i.e: 4d6).")
 
         string = string.split("d")
         num_of_rolls = int(string[0])
         die_type = int(string[1])
 
         if num_of_rolls < 1:
-            raise ValueError("num_of_rolls must be >= 1")
+            raise ValueError("Argument 'string' has an invalid 'num_of_rolls' value.")
 
         if die_type not in (4, 6, 8, 10, 12, 20, 100):
-            raise ValueError("die_type value is invalid")
+            raise ValueError("Argument 'string' has an invalid 'die_type' value.")
 
         for _ in range(0, num_of_rolls):
             yield random.randint(1, die_type)
