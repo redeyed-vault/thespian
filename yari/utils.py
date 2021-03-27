7c1c714f807b75515bf21fe32e1e263eb331596b
mtaylor33 <taylormjm3121@gmail.com>
2021-03-27 15:56:33 -0400

    Added get_subclasses_by_class and sample_choice functions

--- yari/utils.py
+++ yari/utils.py
@@ -1,4 +1,5 @@
 from math import ceil
+from random import sample
 
 from .builder import parser
 
@@ -28,8 +29,14 @@ def get_proficiency_bonus(level: int) -> int:
     return ceil((level / 4) + 1)
 
 
+def get_subclasses_by_class(klass: str) -> list:
+    """Returns a list of subclasses by klass."""
+    return parser.Load.get_columns(klass, "subclasses", source_file="classes")
+
+
 def prompt(message: str, options: (list, tuple)) -> str:
     """
+    User prompt
 
     :param str message:
     :param list,tuple options:
@@ -45,6 +52,16 @@ def prompt(message: str, options: (list, tuple)) -> str:
         return prompt(message, options)
     except RecursionError:
         return ""
+
+
+def sample_choice(choices: list) -> list:
+    """Randomly returns a list of choices."""
+    if len(choices) >= 2:
+        last_value = choices[-1]
+        if isinstance(last_value, int):
+            choices.pop()
+            return sample(choices, last_value)
+    return choices
 
 
 def truncate_dict(dict_to_truncate: dict, ceiling: int) -> dict:
