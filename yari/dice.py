11c37f5756ac2d19a1e19840cdc05cdd7897ddc0
taylormarcus <taylormjm3121@gmail.com>
2021-04-05 02:44:16 -0400

    Unindented code block.

--- yari/dice.py
+++ yari/dice.py
@@ -15,15 +15,15 @@ def roll(string: str):
         if not re.search("[0-9]d[0-9]", string):
             raise ValueError("Argument 'string' has an invalid format (i.e: 4d6).")
 
-        string = string.split("d")
-        num_of_rolls = int(string[0])
-        die_type = int(string[1])
+    string = string.split("d")
+    num_of_rolls = int(string[0])
+    die_type = int(string[1])
 
-        if num_of_rolls < 1:
-            raise ValueError("Argument 'string' has an invalid 'num_of_rolls' value.")
+    if num_of_rolls < 1:
+        raise ValueError("Argument 'string' has an invalid 'num_of_rolls' value.")
 
-        if die_type not in (1, 4, 6, 8, 10, 12, 20, 100):
-            raise ValueError("Argument 'string' has an invalid 'die_type' value.")
+    if die_type not in (1, 4, 6, 8, 10, 12, 20, 100):
+        raise ValueError("Argument 'string' has an invalid 'die_type' value.")
 
-        for _ in range(0, num_of_rolls):
-            yield random.randint(1, die_type)
+    for _ in range(0, num_of_rolls):
+        yield random.randint(1, die_type)
