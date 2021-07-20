03976fbff66bf0bbc3b921d8d9e78f1d811018dd
mtttech <taylor.marcus3121@gmail.com>
2021-07-20 02:36:33 -0400

    Added anthropometric calculator.

--- yari/flags.py
+++ yari/flags.py
@@ -1,6 +1,7 @@
 from abc import ABC, abstractmethod
 from collections import namedtuple
 
+from dice import roll
 from errors import Error
 from sources import Load
 from utils import _e, prompt
@@ -426,7 +427,50 @@ class RaceSeamstress(_DataSet):
         self.data = self.read_dataset(True)
 
 
+class AnthropometricCalculator:
+    def __init__(self, race, sex, subrace=None):
+        self.race = race
+        self.sex = sex
+        self.subrace = subrace
+
+    def _select_base(self):
+        base_values = list()
+        for characteristic in ("height", "weight"):
+            result = Load.get_columns(self.race, characteristic, source_file="ratio")
+            if result is None:
+                result = Load.get_columns(self.subrace, characteristic, source_file="ratio")
+                if result is None:
+                    raise Error("No racial base found for height/weight calculation.")
+            base_values.append(result)
+
+        height,weight = base_values
+        return height, weight
+
+    def calculate(self, factor_sex=False):
+        height_values, weight_values = self._select_base()
+        height_pair = height_values.split(",")
+        weight_pair = weight_values.split(",")
+
+        # Height formula = base + modifier result
+        height_base = int(height_pair[0])
+        height_modifier = sum(list(roll(height_pair[1])))
+        height_calculation = height_base + height_modifier
+
+        # Weight formula = height modifier * weight modifier + base
+        weight_base = int(weight_pair[0])
+        weight_modifier = sum(list(roll(weight_pair[1])))
+        weight_calculation = (weight_modifier * height_modifier) + weight_base
+
+        # Unofficial rule, using real world examples
+        if factor_sex:
+            raise Error("Not yet implemented.")
+
+        return height_calculation, weight_calculation
+
+
 if __name__ == "__main__":
+    a = AnthropometricCalculator("Elf", "Male", "Drow")
+    print(a.calculate())
     a = RaceSeamstress("Elf")
     b = ClassSeamstress("Bard", a.data)
     c = _DataSet()
