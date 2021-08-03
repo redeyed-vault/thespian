8ae55d230f7709b2d432741787be89b495acf768
mtttech <taylor.marcus3121@gmail.com>
2021-08-03 13:15:48 -0400

    Code clean up.

--- yari/core/__init__.py
+++ yari/core/__init__.py
@@ -438,7 +438,12 @@ def main():
     sex = args.sex
     port = args.port
 
-    from .seamstress import RaceSeamstress, ClassSeamstress, AttributeGenerator, MyTapestry
+    from .seamstress import (
+        RaceSeamstress,
+        ClassSeamstress,
+        AttributeGenerator,
+        MyTapestry,
+    )
 
     # Generate racial and class attributes
     a = RaceSeamstress(race, sex)
