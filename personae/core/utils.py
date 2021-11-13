d2f3e733b1dac31703360b9921f6c53f5ad9c39e
mtttech <taylor.marcus3121@gmail.com>
2021-11-13 03:44:39 -0500

    Fixed prompt bug during menu generation.

--- personae/core/utils.py
+++ personae/core/utils.py
@@ -51,46 +51,46 @@ def get_character_races() -> tuple:
     return Load.get_row_ids(source_file="races")
 
 
-def prompt(message, options, selected_options=None):
+def prompt(message, base_opts, selected_opts=None):
     """
     User input prompter function
 
     :param str message:
-    :param list,tuple options:
-    :param list,tuple selected_options:
+    :param list,tuple base_opts:
+    :param list,tuple selected_opts:
 
     """
     import time
 
     time.sleep(2.2)
 
-    if selected_options is not None and not isinstance(selected_options, tuple):
-        selected_options = tuple(selected_options)
+    if selected_opts is not None and not isinstance(selected_opts, tuple):
+        selected_opts = tuple(selected_opts)
 
-    if isinstance(selected_options, (list, tuple)):
-        options = [o for o in options if o not in selected_options]
+    if isinstance(selected_opts, (list, tuple)):
+        base_opts = [o for o in base_opts if o not in selected_opts]
 
-    if not isinstance(options, tuple):
-        options = tuple(options)
+    if not isinstance(base_opts, tuple):
+        base_opts = tuple(base_opts)
 
     message = colored("[P] " + message, "green", attrs=["bold"])
-    options = {x: y for x, y in enumerate(options)}
+    base_opts = {x: y for x, y in enumerate(base_opts)}
 
     options_list = "\n"
-    for id, option in options.items():
+    for id, option in base_opts.items():
         options_list += f"[{id}] {option}\n"
     options_list = colored(options_list, "white", attrs=["bold"])
     options_list += colored("[*]", "green", attrs=["bold"])
 
     try:
         user_value = int(input(f"{message} {options_list} ").strip())
-        if user_value in options:
-            return options[user_value]
-        else:
-            raise ValueError
+        if user_value not in base_opts:
+            raise ValueError()
     except KeyboardInterrupt:
         exit("\nHalting...")
     except ValueError:
-        return prompt(message, options.values(), selected_options)
+        return prompt(message, base_opts.values(), selected_opts)
     except RecursionError:
         return None
+    else:
+        return base_opts[user_value]
