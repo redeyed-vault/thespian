2b29a0e5b5d4757fac43cbdd3be673c037f956f1
mtttech <taylormjm3121@gmail.com>
2022-06-09 21:58:22 -0400

    Incorporating spell lists. Fixed section reference numbers.

--- src/thespian/sourcetree/__init__.py
+++ src/thespian/sourcetree/__init__.py
@@ -1,12 +1,13 @@
-from .dnd5e import dnd5e_sources
+from .dnd5e import dnd5e
 
 
 class SourceTree:
-    backgrounds: object = dnd5e_sources["backgrounds"]
-    classes: object = dnd5e_sources["classes"]
-    feats: object = dnd5e_sources["feats"]
-    metrics: object = dnd5e_sources["metrics"]
-    races: object = dnd5e_sources["races"]
-    skills: object = dnd5e_sources["skills"]
-    subclasses: object = dnd5e_sources["subclasses"]
-    subraces: object = dnd5e_sources["subraces"]
+    backgrounds: object = dnd5e["backgrounds"]
+    classes: object = dnd5e["classes"]
+    feats: object = dnd5e["feats"]
+    metrics: object = dnd5e["metrics"]
+    races: object = dnd5e["races"]
+    skills: object = dnd5e["skills"]
+    spell_lists: object = dnd5e["spell_lists"]
+    subclasses: object = dnd5e["subclasses"]
+    subraces: object = dnd5e["subraces"]
